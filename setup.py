0cf19507d3e2f21b683ced7d8194ecffb7652578
Jimmy He <jimdwq@gmail.com>
2025-04-15 15:22:04 +1200

    Set up files and requirements done

--- setup.py
+++ setup.py
@@ -1,5 +1,30 @@
 # this is used to build the ML project as a package
 from setuptools import find_packages, setup
+from typing import List
+
+
+
+HYPHEN_E_DOT =  '-e .'
+def get_requirements(file_path:str)->List[str]:
+    """
+    this function will return the list of requirements
+    the `requirements.txt` file, excluding the `-e .` entry if it exists.
+
+    """
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]  
+        #replaces the new line sign with blanks
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+
+
+    return requirements
+
 
 
 
@@ -9,7 +34,7 @@ setup(
     author = "Jimmy",
     author_email="jimdwq@gmail.com",
     packages=find_packages(),
-    install_requires = ['pandas','numpy','seaborn']
+    install_requires = get_requirements("requirements.txt")
 )
 
 
